d686936ff0c0f30db24b41c54fa38b748a2c62b5
Eugene Koontz <ekoontz@hiro-tan.org>
2012-12-29 19:51:51 +0000

    add comment about symlink

--- test.sh
+++ test.sh
@@ -1,1 +1,26 @@
-java -cp $HOME/.m2/repository/org/slf4j/slf4j-log4j12/1.6.1/slf4j-log4j12-1.6.1.jar:conf:.:core/target/core-0.1.4.jar:hbase/target/hbase-binding-0.1.4.jar:$HOME/.m2/repository/org/apache/hadoop/hadoop-common/2.1.0.tm6/hadoop-common-2.1.0.tm6.jar:/Users/ekoontz/.m2/repository/org/apache/hbase/hbase/0.94.2.tm6/hbase-0.94.2.tm6.jar com.yahoo.ycsb.Client -db com.yahoo.ycsb.db.HBaseClient -load -p columnfamily=ycsb -P workloads/workloada
+kinit ec2-user
+# HDFS:
+~/hadoop-runtime/bin/hadoop  --config conf  fs  -ls /
+
+#running a MR job:
+~/mr1/bin/hadoop --config /home/ec2-user/YCSB/conf jar ~/mr1/build/hadoop-examples-mr1-2.1.0.tm6.jar  pi 5 5
+
+#hbase shell:
+export HBASE_CONF_DIR=/home/ec2-user/YCSB/conf
+~/hbase/bin/hbase shell
+
+#as hbase admin, in hbase shell:
+## create table 'usertable' with column family 'ycsb':
+create 'usertable','ycsb'
+## allow full access for ec2-user:
+put '_acl_','usertable','info:ec2-user','RWXCA'
+
+# link your actual hbase conf to where YCSB will look for it:
+mv hbase/src/main/conf hbase/src/main/conf-tmp
+ln -s `pwd`/conf hbase/src/main
+
+# back on client (ec2-user):
+bin/ycsb load hbase -p columnfamily=ycsb  -P workloads/workloada
+bin/ycsb run  hbase -p columnfamily=ycsb  -P workloads/workloada
+
+
